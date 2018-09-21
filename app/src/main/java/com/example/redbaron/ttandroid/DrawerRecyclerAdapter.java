4543504abc8eaf065e38cf7ed18723468b1f1c5e
Berin <be.selimotic72@gmail.com>
2018-09-21 13:49:43 -0700

    Added nav drawer settings menu, completed UI on it, debugged it

---
+++ app/src/main/java/com/example/redbaron/ttandroid/DrawerRecyclerAdapter.java
@@ -0,0 +1,103 @@
+package com.example.redbaron.ttandroid;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class DrawerRecyclerAdapter extends RecyclerView.Adapter{
+
+    private static final String TAG = "DrawerRecyclerAdapter";
+
+    private ArrayList<String> settingsSubitemTitles = new ArrayList<>();
+    private ArrayList<String> settingsSubitemDescriptions = new ArrayList<>();
+    private ArrayList<String> settingsSubitemLinks = new ArrayList<>();
+    private Context mContext;
+
+    public DrawerRecyclerAdapter(ArrayList<String> titles, ArrayList<String> descriptions,
+                                 ArrayList<String> links, Context mContext){
+        settingsSubitemDescriptions = descriptions;
+        settingsSubitemLinks = links;
+        settingsSubitemTitles = titles;
+        this.mContext = mContext;
+    }
+
+
+    @NonNull
+    @Override
+    public SettingsItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.drawer_item_layout, parent, false);
+        SettingsItemHolder vHolder = new SettingsItemHolder(v);
+        return vHolder;
+    }
+
+    @Override
+    public void onViewRecycled(@NonNull RecyclerView.ViewHolder holder) {
+        ((SettingsItemHolder) holder).RHSViewContainer.removeAllViews();
+        super.onViewRecycled(holder);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+
+        ((SettingsItemHolder) holder).subitemDescription.setText(settingsSubitemDescriptions.get(position));
+        ((SettingsItemHolder) holder).subitemTitle.setText(settingsSubitemTitles.get(position));
+
+        FrameLayout rightViewContainer = ((SettingsItemHolder) holder).RHSViewContainer;
+        FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        flp.gravity = Gravity.CENTER;
+        if(settingsSubitemLinks.get(position).equals("Switch")){
+            Switch RHSswitch = new Switch(mContext);
+            RHSswitch.setChecked(false);
+            RHSswitch.setLayoutParams(flp);
+            rightViewContainer.addView(RHSswitch);
+        } else {
+            TextView RHStext = new TextView(mContext);
+            RHStext.setText(settingsSubitemLinks.get(position));
+            RHStext.setTextSize(20);
+            RHStext.setTextColor(Color.parseColor("#1348c4"));
+            RHStext.setLayoutParams(flp);
+            RHStext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(mContext, ((SettingsItemHolder) holder).subitemTitle.getText().toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            rightViewContainer.addView(RHStext);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return settingsSubitemTitles.size();
+    }
+
+    public class SettingsItemHolder extends RecyclerView.ViewHolder{
+
+        TextView subitemTitle;
+        TextView subitemDescription;
+        LinearLayout parentLayout;
+        FrameLayout RHSViewContainer;
+
+        public SettingsItemHolder(View itemView) {
+            super(itemView);
+            subitemTitle = itemView.findViewById(R.id.drawer_subitem_name);
+            subitemDescription = itemView.findViewById(R.id.drawer_subitem_description);
+            parentLayout = itemView.findViewById(R.id.drawer_parent);
+            RHSViewContainer = itemView.findViewById(R.id.frame_switch_text);
+        }
+    }
+}
--- app/src/main/java/com/example/redbaron/ttandroid/MainActivity.java
+++ app/src/main/java/com/example/redbaron/ttandroid/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.redbaron.ttandroid;
 
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -13,11 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
     public static ImageView current_state;
     public static int current_opposite;
+    private DrawerLayout mDrawerLayout;
+
+    private ArrayList<String> dwTitles;
+    private ArrayList<String> dwDescriptions;
+    private ArrayList<String> dwLinks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.tt_nav_drawer_container);
+
+        dwTitles = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.dw_titles)));
+        dwDescriptions = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.dw_descriptions)));
+        dwLinks = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.dw_links)));
+
+        initRecyclerView();
+
+        Toolbar actionBar = (Toolbar) findViewById(R.id.tt_top_bar);
+        setSupportActionBar(actionBar);
+
+        ActionBar activeActionBar = (ActionBar) getSupportActionBar();
+        activeActionBar.setDisplayHomeAsUpEnabled(true);
+        activeActionBar.setHomeAsUpIndicator(R.mipmap.ic_nav_button);
 
         current_state = (ImageView) findViewById(R.id.nav_home);
         current_opposite = R.mipmap.ic_home;
@@ -53,5 +82,24 @@ public class MainActivity extends AppCompatActivity {
                 current_opposite = R.mipmap.ic_location;
                 break;
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.tt_drawer_list);
+        DrawerRecyclerAdapter adapter = new DrawerRecyclerAdapter(dwTitles, dwDescriptions, dwLinks, this);
+        DividerItemDecoration itemDecor = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecor);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
